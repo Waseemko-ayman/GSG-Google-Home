49dad9f254ef7a81422b334f42baea9a7bacf0ed
Waseem A. Abd Elhadi <112069422+Waseemko-ayman@users.noreply.github.com>
2023-06-06 11:22:36 +0200

    Add app Icon src in dark mode

--- js/control.js
+++ js/control.js
@@ -1,13 +1,16 @@
 let darkMode = document.querySelector(".mode");
 let body = document.body;
 let googleWord = document.querySelector(".google-word");
+let appIcon = document.querySelector(".app-img");
 
 darkMode.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
 
     if(body.classList.contains("dark-mode")) {
         googleWord.src = "images/google word.png";
+        appIcon.src = "images/appsIconBlack.png";
     } else {
         googleWord.src = "images/google word white.png";
+        appIcon.src = "images/appsIconWhite.png";
     }
 });
